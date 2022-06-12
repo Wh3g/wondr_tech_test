d42e6bc8bf4b8b329d969e1426c0d60496130df1
Willoughby Hood <willoughbyhood29@googlemail.com>
2022-06-12 18:54:06 +0100

    count four of a kind

--- lib/poker.rb
+++ lib/poker.rb
@@ -12,6 +12,40 @@ class Poker
     end
 
     def check_hand(hand)
-        1
+        if straight_flush(hand)
+            1
+        elsif four_of_a_kind(hand)
+            2
+        end
+    end
+
+    def straight_flush(hand)
+        suit = hand[0][:suit]
+        sorted_hand = hand.sort_by { |card| card[:value]}
+        sorted_hand.length.times do | index |
+            if hand[index][:suit] != suit
+                return false
+            elsif hand[index][:value] != (hand[index - 1][:value] + 1) && index != 0
+                hand[index][:value]
+                hand[index - 1][:value]
+                return false
+            end
+        end
+        return true
+    end
+
+    def four_of_a_kind(hand)
+        hand.each do | card1 |
+            count = 0
+            value = card1[:value]
+            hand.each do | card2 |
+                if card2[:value] == value
+                    count += 1
+                end
+                if count == 4
+                    true
+                end
+            end
+        end
     end
 end
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -3,7 +3,8 @@ require 'poker'
 describe Poker do
     before do
         @example_hands = [
-            {"suit": "heart", "value": 1}, {"suit": "heart", "value": 13}, {"suit": "heart", "value": 12}, {"suit": "heart", "value": 11}, {"suit": "heart", "value": 10}
+            [{"suit": "heart", "value": 9}, {"suit": "heart", "value": 10}, {"suit": "heart", "value": 11}, {"suit": "heart", "value": 12}, {"suit": "heart", "value": 13}],
+            [{"suit": "heart", "value": 1}, {"suit": "spade", "value": 1}, {"suit": "club", "value": 1}, {"suit": "diamond", "value": 1}, {"suit": "heart", "value": 10}]
         ]
     end
     describe "#load_hands" do
@@ -17,6 +18,10 @@ describe Poker do
     describe "#check_hand" do
         it "checks straight flushes" do
             expect(subject.check_hand(@example_hands[0])).to eq 1
+        end
+
+        it "checks 4 of a kind" do
+            expect(subject.check_hand(@example_hands[1])).to eq 2
         end
     end
 end
